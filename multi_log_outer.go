4a65eebf2d56d4fc0e8e902d5f3f440d8170fa50
Alex Reece <awreece@gmail.com>
2011-10-08 04:01:38 -0400

    Export MulitLogOuter

--- multi_log_outer.go
+++ multi_log_outer.go
@@ -6,16 +6,16 @@ import (
 	"os"
 )
 
-type multiLogOuter struct {
+type MultiLogOuter struct {
 	// TODO Add mutex.
 	outers map[string]LogOuter
 }
 
-func (l *multiLogOuter) String() string {
+func (l *MultiLogOuter) String() string {
 	return fmt.Sprint("\"", l.outers, "\"")
 }
 
-func (l *multiLogOuter) Set(name string) bool {
+func (l *MultiLogOuter) Set(name string) bool {
 	if file, err := os.Create(name); err != nil {
 		os.Stderr.WriteString(
 			fmt.Sprint("Error opening file for logging", name,
@@ -29,31 +29,31 @@ func (l *multiLogOuter) Set(name string) bool {
 	panic("Code never reaches here, this mollifies the compiler.")
 }
 
-func (l *multiLogOuter) AddLogOuter(key string, outer LogOuter) {
+func (l *MultiLogOuter) AddLogOuter(key string, outer LogOuter) {
 	// TODO Grab mutex.
 	l.outers[key] = outer
 }
 
-func (l *multiLogOuter) RemoveLogOuter(key string) {
+func (l *MultiLogOuter) RemoveLogOuter(key string) {
 	// TODO Be Go1 compatible. :)
 	l.outers[key] = nil, false
 }
 
-func (l *multiLogOuter) Output(m *LogMessage) {
+func (l *MultiLogOuter) Output(m *LogMessage) {
 	// TODO Grab mutex.
 	for _, outer := range l.outers {
 		outer.Output(m)
 	}
 }
 
-func (l *multiLogOuter) FailNow() {
+func (l *MultiLogOuter) FailNow() {
 	// TODO Grab mutex.
 	for _, outer := range l.outers {
 		outer.FailNow()
 	}
 }
 
-var defaultLogOuters multiLogOuter = multiLogOuter{make(map[string]LogOuter)}
+var defaultLogOuters MultiLogOuter = MultiLogOuter{make(map[string]LogOuter)}
 
 func init() {
 	flag.Var(&defaultLogOuters, "vlog.logfile", "Log to given file - can "+
