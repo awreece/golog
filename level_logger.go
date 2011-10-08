eb6ea266a908473c5d4bc6ffc16c1c8683eb052d
Alex Reece <awreece@gmail.com>
2011-10-07 20:27:37 -0400

    Add all the extra stubs

--- level_logger.go
+++ level_logger.go
@@ -25,3 +25,42 @@ func (l *LevelLogger) Infof(f string, args ...interface{}) {
 func (l *LevelLogger) Infoc(closure func() string) {
 	l.logCommon(INFO, closure)
 }
+
+func (l *LevelLogger) Warning(vals ...interface{}) {
+	l.logCommon(WARNING, func() string { return fmt.Sprint(vals...) })
+}
+
+func (l *LevelLogger) Warningf(f string, args ...interface{}) {
+	l.logCommon(WARNING, func() string { return fmt.Sprintf(f, args...) })
+}
+
+func (l *LevelLogger) Warningc(closure func() string) {
+	l.logCommon(WARNING, closure)
+}
+
+func (l *LevelLogger) Error(vals ...interface{}) {
+	l.logCommon(ERROR, func() string { return fmt.Sprint(vals...) })
+}
+
+func (l *LevelLogger) Errorf(f string, args ...interface{}) {
+	l.logCommon(ERROR, func() string { return fmt.Sprintf(f, args...) })
+}
+
+func (l *LevelLogger) Errorc(closure func() string) {
+	l.logCommon(ERROR, closure)
+}
+
+func (l *LevelLogger) Fatal(vals ...interface{}) {
+	l.logCommon(FATAL, func() string { return fmt.Sprint(vals...) })
+	l.FailNow()
+}
+
+func (l *LevelLogger) Fatalf(f string, args ...interface{}) {
+	l.logCommon(FATAL, func() string { return fmt.Sprintf(f, args...) })
+	l.FailNow()
+}
+
+func (l *LevelLogger) Fatalc(closure func() string) {
+	l.logCommon(FATAL, closure)
+	l.FailNow()
+}
