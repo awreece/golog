6e8f14d4aed5cb63b1a17b8fd2516ec4ecce60e3
Alex Reece <awreece@gmail.com>
2011-10-07 23:07:57 -0400

    Move logger code from log outer

--- log_outer.go
+++ log_outer.go
@@ -9,16 +9,8 @@
 package golog
 
 import (
-	"flag"
-	"fmt"
 	"os"
 )
-
-// Prints everything this level and above. (Set to SILENT to disable).
-var flag_minloglevel = flag.Int("vlog.minloglevel", INFO,
-	"Log messages at or above this level. The "+
-		"numbers of severity levels INFO, WARNING, "+
-		"ERROR, and FATAL are 0, 1, 2, and 3, respectively")
 
 type LogOuter interface {
 	// Println guarantees a newline and flushes output.
@@ -71,61 +63,4 @@ func (t* testLogOuter) Println(s string) {
 			t.Log(s)
 		}
 	}
-}
-
-type internalLogOuter struct {
-	LogOuter
-	// This is a reference type so we can add log writers before having 
-	// parsed flag_minloglevel.
-	minloglevel *int
-	vmoduleLevelsMap
-}
-
-type logOuterList struct{
-	// TODO Insert mutex here.
-	outers []*internalLogOuter
-}
-
-func (l *logOuterList) String() string {
-	return fmt.Sprint("\"", l.outers, "\"")
-}
-
-func (l *logOuterList) Set(name string) bool {
-	if file, err := os.Create(name); err != nil {
-		os.Stderr.WriteString(
-			fmt.Sprint("Error opening file for logging", name,
-				": ", err))
-		return false
-	} else {
-		l.AddDefaultLogFile(name, file)
-		return true
-	}
-
-	panic("Code never reaches here, this mollifies the compiler.")
-}
-
-// TODO only require filename.
-func (l *logOuterList) AddDefaultLogFile(filename string, file *os.File) {
-	// TODO Grab mutex.
-	l.outers = append(l.outers, &internalLogOuter{
-		LogOuter: &fileLogOuter{file},
-		minloglevel: flag_minloglevel,
-		vmoduleLevelsMap: flag_vmodule,
-	})
-}
-
-func (l *logOuterList) AddDefaultLogTester(t TestController) {
-	// TODO Grab Mutex
-	l.outers = append(l.outers, &internalLogOuter{
-		LogOuter: &testLogOuter{t},
-		minloglevel: flag_minloglevel,
-		vmoduleLevelsMap: flag_vmodule,
-	})
-}
-
-var LogOuters logOuterList
-
-func init() {
-	flag.Var(&LogOuters, "vlog.logfile", "Log to given file - can be"+
-		" provided multiple times to log to multiple files")
 }
--- logger.go
+++ logger.go
@@ -1,5 +1,17 @@
 package golog
 
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+// Prints everything this level and above. (Set to SILENT to disable).
+var flag_minloglevel = flag.Int("vlog.minloglevel", INFO,
+	"Log messages at or above this level. The "+
+		"numbers of severity levels INFO, WARNING, "+
+		"ERROR, and FATAL are 0, 1, 2, and 3, respectively")
+
 // Logger.Log uses the level to determine whether or not to output the
 // arguments. Logger.Log will output the provided arguments exactly, without
 // additional formatting such as adding a prefix etc. In addition, Logger.Log
@@ -17,4 +29,61 @@ type Logger interface {
 type FailLogger interface {
 	Logger
 	FailNow()
+}
+
+type loggerImpl struct {
+	LogOuter
+	// This is a reference type so we can add log writers before having 
+	// parsed flag_minloglevel.
+	minloglevel *int
+	vmoduleLevelsMap
+}
+
+type logOuterList struct {
+	// TODO Insert mutex here.
+	outers []*loggerImpl
+}
+
+func (l *logOuterList) String() string {
+	return fmt.Sprint("\"", l.outers, "\"")
+}
+
+func (l *logOuterList) Set(name string) bool {
+	if file, err := os.Create(name); err != nil {
+		os.Stderr.WriteString(
+			fmt.Sprint("Error opening file for logging", name,
+				": ", err))
+		return false
+	} else {
+		l.AddDefaultLogFile(name, file)
+		return true
+	}
+
+	panic("Code never reaches here, this mollifies the compiler.")
+}
+
+// TODO only require filename.
+func (l *logOuterList) AddDefaultLogFile(filename string, file *os.File) {
+	// TODO Grab mutex.
+	l.outers = append(l.outers, &loggerImpl{
+		LogOuter:         &fileLogOuter{file},
+		minloglevel:      flag_minloglevel,
+		vmoduleLevelsMap: flag_vmodule,
+	})
+}
+
+func (l *logOuterList) AddDefaultLogTester(t TestController) {
+	// TODO Grab Mutex
+	l.outers = append(l.outers, &loggerImpl{
+		LogOuter:         &testLogOuter{t},
+		minloglevel:      flag_minloglevel,
+		vmoduleLevelsMap: flag_vmodule,
+	})
+}
+
+var LogOuters logOuterList
+
+func init() {
+	flag.Var(&LogOuters, "vlog.logfile", "Log to given file - can be"+
+		" provided multiple times to log to multiple files")
 }
