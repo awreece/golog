b4398e1d4272d54ce95abb27266e191c0eb9844e
Alex Reece <awreece@gmail.com>
2011-10-07 20:05:33 -0400

    Started stubbing level_logger.go

---
+++ level_logger.go
@@ -0,0 +1,26 @@
+package golog
+
+import (
+	"fmt"
+)
+
+type LevelLogger struct {
+	Logger
+}
+
+func (l *LevelLogger) logCommon(level int, closure func() string) {
+	// TODO: Add prefix, timestamp, etc.
+	l.Logc(level, closure)
+}
+
+func (l *LevelLogger) Info(vals ...interface{}) {
+	l.logCommon(INFO, func() string { return fmt.Sprint(vals...) })
+}
+
+func (l *LevelLogger) Infof(f string, args ...interface{}) {
+	l.logCommon(INFO, func() string { return fmt.Sprintf(f, args...) })
+}
+
+func (l *LevelLogger) Infoc(closure func() string) {
+	l.logCommon(INFO, closure)
+}
