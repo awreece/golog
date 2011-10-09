618ec0c61fa920d36635179c69d0506f3e42ad0f
Alex Reece <awreece@gmail.com>
2011-10-09 18:29:49 -0400

    Package logger doesn't need to store pack

--- package_logger.go
+++ package_logger.go
@@ -1,12 +1,11 @@
 package golog
 
 type PackageLogger struct {
-	pack string
 	LevelLogger
 }
 
 func NewDefaultPackageLogger(pack string) *PackageLogger {
-	return &PackageLogger{pack, &levelLoggerImpl{
+	return &PackageLogger{&levelLoggerImpl{
 		DefaultLogger,
 		func(int) *LogLocation { return &LogLocation{Package: pack} },
 	}}
