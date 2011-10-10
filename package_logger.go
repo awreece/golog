95e7e10eb0162928e147c32225f38151f19a311a
Alex Reece <awreece@gmail.com>
2011-10-10 18:46:51 -0400

    Comment pacakge logger wrapper functions

--- package_logger.go
+++ package_logger.go
@@ -51,41 +51,71 @@ func (l *PackageLogger) StopTestLogging() {
 	// TODO(awreece) Restored to saved failFunc.
 	l.failFunc = ExitError
 }
+
+// Log the message at level INFO, only formatting if message will be logged.
 func (l *PackageLogger) Info(msg ...interface{}) {
 	l.LogDepth(INFO, printClosure(msg...), 1)
 }
+
+// Log the message at level INFO, only fomatting if message will be logged.
 func (l *PackageLogger) Infof(fmt string, vals ...interface{}) {
 	l.LogDepth(INFO, printfClosure(fmt, vals...), 1)
 }
+
+// Log the message at level INFO, only evaluating the closure if the message
+// will be logged.
 func (l *PackageLogger) Infoc(closure func() string) {
 	l.LogDepth(INFO, closure, 1)
 }
+
+// Log the message at level WARNING, only formatting if message will be logged.
 func (l *PackageLogger) Warning(msg ...interface{}) {
 	l.LogDepth(WARNING, printClosure(msg...), 1)
 }
+
+// Log the message at level WARNING, only fomatting if message will be logged.
 func (l *PackageLogger) Warningf(fmt string, vals ...interface{}) {
 	l.LogDepth(WARNING, printfClosure(fmt, vals...), 1)
 }
+
+// Log the message at level WARNING, only evaluating the closure if the message
+// will be logged.
 func (l *PackageLogger) Warningc(closure func() string) {
 	l.LogDepth(WARNING, closure, 1)
 }
+
+// Log the message at level ERROR, only formatting if message will be logged.
 func (l *PackageLogger) Error(msg ...interface{}) {
 	l.LogDepth(ERROR, printClosure(msg...), 1)
 }
+
+// Log the message at level ERROR, only formatting if message will be logged.
 func (l *PackageLogger) Errorf(fmt string, vals ...interface{}) {
 	l.LogDepth(ERROR, printfClosure(fmt, vals...), 1)
 }
+
+// Log the message at level ERROR, only evaluating the closure if the message
+// will be logged.
 func (l *PackageLogger) Errorc(closure func() string) {
 	l.LogDepth(ERROR, closure, 1)
 }
+
+// Log the message at level FATAL, only formatting if the message will be
+// logged. Also call l.FailNow().
 func (l *PackageLogger) Fatal(msg ...interface{}) {
 	l.LogDepth(FATAL, printClosure(msg...), 1)
 	l.FailNow()
 }
+
+// Log the message at level FATAL, only formatting if the message will be
+// logged. Also call l.FailNow().
 func (l *PackageLogger) Fatalf(fmt string, vals ...interface{}) {
 	l.LogDepth(FATAL, printfClosure(fmt, vals...), 1)
 	l.FailNow()
 }
+
+// Log the message at level FATAL, only evaluating the clousre if the message
+// will be logged. Also call l.FailNow().
 func (l *PackageLogger) Fatalc(closure func() string) {
 	l.LogDepth(FATAL, closure, 1)
 	l.FailNow()
