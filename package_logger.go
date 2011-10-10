f8750518ce3b796d12a1912875985fcc4f450a8e
Alex Reece <awreece@gmail.com>
2011-10-10 00:48:06 -0400

    add more customizable package_logger constructor

--- package_logger.go
+++ package_logger.go
@@ -12,21 +12,27 @@ type packageLoggerImpl struct {
 }
 
 func NewDefaultPackageLogger() PackageLogger {
-	ret := &packageLoggerImpl{ failFunc: exitNow }
+	return NewPackageLogger(
+		NewDefaultMultiLogOuter(),
+		flag_minloglevel,
+		exitNow,
+		FullLocation)
+}
+
+func NewPackageLogger(outer LogOuter, minloglevel_flag *int,
+failFunc func(), locFunc func(skip int) *LogLocation) PackageLogger {
+	ret := &packageLoggerImpl{failFunc: failFunc}
 
 	ret.LevelLogger = NewLevelLogger(
-		&loggerImpl{
-			NewDefaultMultiLogOuter(),
-			flag_minloglevel,
-			func() { ret.failFunc() },
-		}, FullLocation)
+		&loggerImpl{outer, minloglevel_flag, func() { ret.failFunc() }},
+		FullLocation)
 
 	return ret
 }
 
 func (l *packageLoggerImpl) StartTestLogging(t TestController) {
 	defaultLogOuters.AddLogOuter("testing", NewTestLogOuter(t))
-	l.failFunc = func () { t.FailNow() }
+	l.failFunc = func() { t.FailNow() }
 }
 
 func (l *packageLoggerImpl) StopTestLogging() {
