5c6b06fcc0e5ed9af7f3b19e48ff912102ec8eac
Alex Reece <awreece@gmail.com>
2011-10-10 01:07:39 -0400

    packageLoggerImpl embeds a MultitLogOuter

--- package_logger.go
+++ package_logger.go
@@ -4,10 +4,13 @@ type PackageLogger interface {
 	LevelLogger
 	StartTestLogging(TestController)
 	StopTestLogging()
+	AddLogOuter(key string, outer LogOuter)
+	RemoveLogOuter(key string)
 }
 
 type packageLoggerImpl struct {
 	LevelLogger
+	MultiLogOuter
 	failFunc func()
 }
 
@@ -19,9 +22,9 @@ func NewDefaultPackageLogger() PackageLogger {
 		FullLocation)
 }
 
-func NewPackageLogger(outer LogOuter, minloglevel_flag *int,
+func NewPackageLogger(outer MultiLogOuter, minloglevel_flag *int,
 failFunc func(), locFunc func(skip int) *LogLocation) PackageLogger {
-	ret := &packageLoggerImpl{failFunc: failFunc}
+	ret := &packageLoggerImpl{failFunc: failFunc, MultiLogOuter: outer}
 
 	ret.LevelLogger = NewLevelLogger(
 		&loggerImpl{outer, minloglevel_flag, func() { ret.failFunc() }},
@@ -31,11 +34,11 @@ failFunc func(), locFunc func(skip int) *LogLocation) PackageLogger {
 }
 
 func (l *packageLoggerImpl) StartTestLogging(t TestController) {
-	defaultLogOuters.AddLogOuter("testing", NewTestLogOuter(t))
+	l.MultiLogOuter.AddLogOuter("testing", NewTestLogOuter(t))
 	l.failFunc = func() { t.FailNow() }
 }
 
 func (l *packageLoggerImpl) StopTestLogging() {
-	defaultLogOuters.RemoveLogOuter("testing")
+	l.MultiLogOuter.RemoveLogOuter("testing")
 	l.failFunc = exitNow
 }
