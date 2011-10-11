b4efcf4196547206634327f89bae12fc47a517ec
Alex Reece <awreece@gmail.com>
2011-10-11 03:23:53 -0400

    Add TestOutputSameLevel

--- logger_test.go
+++ logger_test.go
@@ -5,7 +5,21 @@ import (
 	"testing"
 )
 
+
 func TestOutput(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	message := &LogMessage{Level: 1}
+
+	mockLogOuter := NewMockLogOuter(mockCtrl)
+	mockLogOuter.EXPECT().Output(message)
+
+	logger := NewLogger(mockLogOuter, 0, nil)
+	logger.Log(1, func() *LogMessage { return message })
+}
+
+func TestOutputSameLevel(t *testing.T) {
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
 
